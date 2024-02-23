7057e48a1a9f5aa6942a8cac3b469fc71bbeeac9
leonardo060190 <leonardo-morruga@hotmail.com>
2024-02-22 21:27:50 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()); // Para parsing de JSON
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
